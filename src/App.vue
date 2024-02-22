bb89e318831fe0ead95343a77577c43489ad26ba
adamGT <adamgetachew55@gmail.com>
2024-02-22 01:33:13 -0800

    its a few minutes  work so there wont be too much commits

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app >
+      <v-main>
+        <HelloWorld/>
+      </v-main>
+        <br>
+        <br>
+        <br>
+      <v-footer color="#1a6aa7" class=" d-flex flex-column">
+        <div class=" d-flex w-100 align-center px-4">
+          <strong class="white--text">Blue Health Ethiopia</strong>
+        </div>
+
+        <div class="white--text px-4 py-2 bg-black text-center w-100">
+          <strong>Copyright © 2024</strong>
+        </div>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+
+};
+</script>
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #013A65; 
+}
+.foot{
+  background-color: #FFCC00; 
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,163 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <a href="https://www.bluehealthethiopia.com" target="_blank">
+        <v-img
+          src="../assets/bluehealth.png"
+          class="my-3"
+          contain
+          height="100"
+        />
+        </a>
+      </v-col>
+
+      <v-col class="mb-4">
+        <h3 class="white--text display-1 font-weight mb-1" >
+          Welcome to
+        </h3>
+        <h3 class="white--text display-1 font-weight-bold mb-3">
+          Blue Health Ethiopia
+        </h3>
+
+        <p class="white--text subheading font-weight-regular">
+          Thank you for your interest to contact us
+          <br>you can reach us on any of the following social media. 
+        </p>
+
+
+        <v-row>
+          <v-col>
+    
+            <a href="https://www.linkedin.com/company/blue-health-ethiopia" target="_blank">
+            <v-card  color="#005983" class="socialcard mx-auto px-6 py-8 mb-2" max-width="390" height="100">
+              <h3 class="facebooktxt display-1 font-weight-bold mb-3">
+                Linkedin
+              </h3>
+            </v-card>
+            </a>
+
+            <a href="https://www.instagram.com/bluehealthethiopia" target="_blank">
+            <v-card class="instagram socialcard mx-auto px-6 py-8 mb-2" max-width="390" height="100">
+              <h3 class="facebooktxt display-1 font-weight-bold mb-3">
+                Instagram
+              </h3>
+            </v-card>
+            </a>
+
+            <a href="https://www.tiktok.com/@bluehealthethiopia" target="_blank">
+            <v-card color="#000000" class="socialcard mx-auto px-6 my-5 mb-2" max-width="390" height="100">
+              <v-row class="text-center">
+                <v-col cols="6">
+                  <v-img
+                    src="../assets/tiktok.png"
+                    contain
+                    height="80"
+                  />
+                </v-col>
+                <v-col cols="6">
+                  <h3 class="tiktoktext display-1 font-weight-bold mt-6 mb-3">
+                    TikTok
+                  </h3>
+                </v-col>
+              </v-row>
+            </v-card>
+            </a>
+
+            <a href="https://www.youtube.com/@bluehealthethiopia" target="_blank">
+              <v-card color="#FF0000" class="socialcard mx-auto px-6 py-2 mb-2" max-width="390" height="100">
+                    <v-img
+                      src="../assets/yt.png"
+                      contain
+                      height="80"
+                    />
+              </v-card>
+            </a>
+          </v-col>
+
+          <v-col>
+            
+
+            <a href="https://t.me/bluehealthethiopia" target="_blank">
+            <v-card  color="#0A739F" class="socialcard mx-auto px-6 py-8 mb-2" max-width="390" max-height="100">
+              <h3 class="facebooktxt display-1 font-weight-bold mb-3">
+                Telegram
+              </h3>
+            </v-card>
+            </a>
+
+            <a href="https://www.facebook.com/bluehealthethiopia" target="_blank">
+              <v-card color="#485A96" class="socialcard mx-auto px-6 py-8 mb-2" max-width="390" height="100">
+                <h3 class="facebooktxt display-1 font-weight-bold mb-3">
+                  Facebook
+                </h3>
+              </v-card>
+            </a>
+            <a href="https://twitter.com/bluehealthethiopia" target="_blank">
+              <v-card color="#000000" class="socialcard mx-auto px-6 py-4 mb-2" max-width="390" height="100">
+                  <v-img
+                    src="../assets/xpng.png"
+                    contain
+                    height="80"
+                  />
+              </v-card>
+            </a>
+
+            <a href="hhttps://www.bluehealthethiopia.com" target="_blank">
+              <v-card color="#FFCC00" class="socialcard   mx-auto px-6 py-8 mb-2" max-width="390" height="100">
+                <h3 class="facebooktxt display-1 font-weight-bold mb-3">
+                  Check our website
+                </h3>
+              </v-card>
+            </a>
+          </v-col>
+        </v-row>
+
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+    data: () => ({
+    }),
+  }
+</script>
+<style>
+.facebooktxt {
+  /* color: #136587;  */
+  color: #FFFFFF; 
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.websitetext {
+  /* color: #136587;  */
+  color: #FFCC00; 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.instagram{
+  background: #f09433; 
+  background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%); 
+  background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+  background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%); 
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
+  }
+
+.tiktoktext{
+    text-align: start;
+    color: #FFFFFF; 
+  }
+
+.socialcard:hover {
+  filter: drop-shadow(0 0 0px rgb(255, 255, 255, 60%));
+  color: rgb(255, 255, 255, 80%);
+  height: 95px;
+  width: 370px;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
 }
+
 a {
-  color: #42b983;
+  text-decoration: none;
 }
 </style>
